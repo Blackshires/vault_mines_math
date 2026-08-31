983ca4b24247d09312eecee9313c0a4a054722c0
Blackshires <shires@hotmail.fr>
2026-08-31 08:04:45 +0200

    Add Vault Mines executable layer

---
+++ game_executables.py
@@ -0,0 +1,9 @@
+"""Vault Mines execution helpers."""
+
+from game_calculations import GameCalculations
+
+
+class GameExecutables(GameCalculations):
+    """Execution layer for Vault Mines."""
+
+    pass
