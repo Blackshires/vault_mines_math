9f23cd4d152bc6b125ca69abcc5a54120b633ef2
Blackshires <shires@hotmail.fr>
2026-08-31 08:13:12 +0200

    Implement required special symbol hook

--- game_override.py
+++ game_override.py
@@ -10,3 +10,7 @@ class GameStateOverride(GameExecutables):
         super().reset_book()
         self.mine_count = 3
         self.safe_target = 1
+
+    def assign_special_sym_function(self):
+        """Vault Mines has no reel symbols or special-symbol assignment."""
+        pass
